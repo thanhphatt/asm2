c85329cb6372040b8415d261a9e70434d00246e5
phattnt <thanhphat200415@gmail.com>
2024-06-13 18:07:23 +0700

    hj

--- FE/src/app/admin/User/user.component.ts
+++ FE/src/app/admin/User/user.component.ts
@@ -29,5 +29,28 @@ export class UsersComponent implements OnInit {
   viewUserDetail(id: string) {
     this.router.navigate(['/user', id]);
   }
+  deleteUser(id: string) {
+    // Hỏi người dùng có chắc chắn muốn xóa không
+    const confirmation = confirm('Bạn có chắc chắn muốn xóa người dùng này không?');
+    
+    // Nếu người dùng chọn "OK", tiếp tục xóa
+    if (confirmation) {
+      this.userService.deleteuser(id).subscribe(
+        response => {
+          console.log('User deleted:', response); // Debugging line
+          // Cập nhật danh sách người dùng sau khi xóa
+          this.users = this.users.filter(user => user._id.$oid !== id);
+          // Load lại trang
+          this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+            this.router.navigate(['/user']);
+          });
+        },
+        error => console.error('Lỗi khi xóa người dùng:', error)
+      );
+    } else {
+      // Nếu người dùng chọn "Cancel", không làm gì cả
+      console.log('Hủy xóa người dùng');
+    }
+  }
 
 }
